9266f05daf71e648745454bb3147df7bb4839954
javier06herrera <37386140+javier06herrera@users.noreply.github.com>
2022-04-20 18:47:46 -0600

    Update streamlit_app.py
    Several lines added to the page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as sl
 
 sl.title("My parents new healthy Diner")
+sl.header("Breakfast Menu")
+sl.text("Omega 3 & Blueberry Oatmeal")
+sl.text("Kale, Spinach and Rocket Smoothie")
+sl.text("Hard-Boiled Free-Range Egg")
